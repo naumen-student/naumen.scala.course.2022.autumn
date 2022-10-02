ec5975f2b8474396dc31cd667f8663e316183bea
Grigorij_Kosov <kosov.gr@mail.ru>
2022-10-02 21:17:08 +0600

    Revert "Add task_3 folder"
    This reverts commit 94ba1b93722e120b59c1336f0ec36d076c6ef538.

--- homeworks/homework_1/task_3/src/main/scala/example/Main.scala
+++
@@ -1,9 +0,0 @@
-package example
-
-object Main extends App{
-  val greeting = (helloVersion: String, name: String) => println(s"$helloVersion Scala!This is $name")
-  val greetingVersions: List[String] = List("Hello", "Hola", "Guten Tag")
-  val name: String = "Grisha"
-  for (version <- greetingVersions) greeting(version, name)
-  for (version <- greetingVersions) greeting(version, name.reverse)
-}
