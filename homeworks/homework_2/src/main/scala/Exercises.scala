1fd88c87218a44ed091ad7e22483b059706c4279
Ekaterina <katya.panteleeva.2018@inbox.ru>
2022-10-04 18:23:09 +0500

    homework_2 Пантелеева Екатерина

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,8 +16,14 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
-
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        var sum = 0
+        for (i <- iFrom to iTo) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i
+        }
+        sum
+    }
 
 
     /*ЗАДАНИЕ II*/
@@ -25,7 +31,21 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        var num = number
+        var sequencer = 2
+        var res: Seq[Int] = Seq(1)
+        while (num > 1) {
+            if (num % sequencer == 0) {
+                num /= sequencer
+                res :+= sequencer
+            }
+            else {
+                sequencer += 1
+            }
+        }
+        res
+    }
 
 
 
@@ -40,15 +60,16 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D) : Double =
+        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
+
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
+
 
 
 
@@ -71,6 +92,12 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def getMass(r: Int, density: Double): Double = {
+        4 / 3 * java.lang.Math.PI * r * r * r * density
+    }
+
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        ballsArray.toSeq.sortBy(ball => getMass(ball._2._1, ball._2._2)).map(_._1)
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,12 +1,40 @@
+import Exercises.Vector2D
 import utest._
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests = Tests{
+    val tests = Tests {
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
             assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
         }
+
+        'sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(3, 10) == 33)
+            assert(Exercises.sumOfDivBy3Or5(11, 25) == 135)
+            assert(Exercises.sumOfDivBy3Or5(50, 55) == 210)
+        }
+
+        'sumByFunc - {
+            assert((Exercises.sumScalars(Vector2D(0.0, 0.0), Vector2D(0.0, 0.0), Vector2D(1.0, 1.0), Vector2D(1.0, 1.0)) - 2.0).abs < 0.01)
+            assert((Exercises.sumScalars(Vector2D(0.0, 0.0), Vector2D(0.0, 0.0), Vector2D(-1.0, -1.0), Vector2D(1.0, 1.0)) + 2.0).abs < 0.01)
+            assert((Exercises.sumCosines(Vector2D(1.0, 1.0), Vector2D(1.0, 1.0), Vector2D(1.0, 1.0), Vector2D(1.0, 1.0)) - 2.0).abs < 0.01)
+            assert((Exercises.sumCosines(Vector2D(1.0, 0.0), Vector2D(0.0, 1.0), Vector2D(0.0, 1.0), Vector2D(1.0, 0.0))).abs < 0.01)
+        }
+
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(69) == Seq(1, 3, 23))
+            assert(Exercises.primeFactor(76) == Seq(1, 2, 2, 19))
+            assert(Exercises.primeFactor(54) == Seq(1, 2, 3, 3, 3))
+        }
+
+        'test_sortByHeavyweight - {
+            assert(Exercises.sortByHeavyweight() == Seq("Tin",
+                "Platinum", "Nickel", "Aluminum", "Titanium", "Lead", "Sodium", "Uranium", "Gold", "Tungsten",
+                "Zirconium", "Chrome", "Iron", "Copper", "Silver", "Plutonium", "Cobalt", "Cesium", "Calcium",
+                "Lithium", "Magnesium", "Potassium", "Graphite"))
+        }
+
     }
 }
