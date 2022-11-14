b3e3d016c71f8ed80ef41e86b57e247f304b50d7
Grigorij_Kosov <kosov.gr@mail.ru>
2022-11-14 17:53:53 +0500

    Done

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -9,15 +9,38 @@ object Exercises {
 
 
 
-  case class Shelter ...
+  case class Shelter[+T <: Animal](animals: List[T]) {
 
+    val _animals: List[T] = animals
+    def +[C >: T <: Animal](animal: C): Shelter[C] = Shelter(_animals :+ animal)
 
+    def ++[C >: T <: Animal](other: Shelter[C]): Shelter[C] = Shelter(_animals ++ other._animals)
 
-  trait Food ...
+    def getNames: List[String] = {
+      _animals.map(x => x.name)
+    }
 
-  case object Meat extends Food[Animal] ...
+    def feed[D <: Animal](food: Food[D]): List[String] = {
+      for (animal <- _animals if (animal.isInstanceOf[D]))
+        yield animal.name + " eats " + food.name
+    }
+  }
 
-  case object Milk extends Food[Cat] ...
+//  то, что требуется сделать во втором задании для прохождения тестов и то,
+//  что написано в md файле - две очень разные вещи
+  trait Food[T <: Animal] {
+    val name: String
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Meat extends Food[Animal]{
+    override val name: String = "meat"
+  }
+
+  case object Milk extends Food[Cat]{
+    override val name: String = "milk"
+  }
+
+  case object Bread extends Food[Dog]{
+    override val name: String = "bread"
+  }
 }
