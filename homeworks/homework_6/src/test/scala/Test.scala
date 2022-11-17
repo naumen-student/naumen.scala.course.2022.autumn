01ea84d379351a48bc4d22225c9199ce2e47e6f6
i.bashkarov <i.bashkarov@tinkoff.ru>
2022-11-17 18:56:51 +0500

    homework_6

--- homeworks/homework_6/src/test/scala/Test.scala
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -4,27 +4,27 @@ import utest._
 object Test extends TestSuite{
 
   val tests = Tests{
-    'reverse - {
+    test("reverse") {
       assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
       assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
     }
 
-    'fibonacci4Index - {
+    test("fibonacci4Index") {
       assert(Exercises.fibonacci4Index(2) == 1)
       assert(Exercises.fibonacci4Index(5) == 5)
     }
 
-    'fibonacci - {
+    test("fibonacci") {
       assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
       assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
     }
 
-    'morse - {
+    test("morse") {
       assert(Exercises.morse("SOS") == "... --- ...")
       assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
     }
 
-    'wordReverse - {
+    test("wordReverse") {
       assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
     }
 
