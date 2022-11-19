348cddb4da2cda9dbb770a5f6f49577830e5b10d
artem <tolstoguzov@tochka.com>
2022-11-19 12:48:53 +0500

    done

--- homeworks/homework_6/src/test/scala/Test.scala
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -1,9 +1,8 @@
 import utest._
 
+object Test extends TestSuite {
 
-object Test extends TestSuite{
-
-  val tests = Tests{
+  val tests = Tests {
     'reverse - {
       assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
       assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
@@ -21,13 +20,20 @@ object Test extends TestSuite{
 
     'morse - {
       assert(Exercises.morse("SOS") == "... --- ...")
-      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
+      assert(
+        Exercises.morse(
+          "Hello world!"
+        ) == ".... . .-.. .-.. ---   .-- --- .-. .-.. -.. !"
+      )
     }
 
     'wordReverse - {
-      assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
+      assert(
+        Exercises.wordReverse(
+          "Зима!.. Крестьянин, торжествуя..."
+        ) == "Амиз!.. Ниняьтсерк, яувтсежрот..."
+      )
     }
-
 
   }
 }
