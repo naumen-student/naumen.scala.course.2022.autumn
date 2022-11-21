d3ed90fdada7ef471b5414fa96e558a6e65a7b63
Dennis <miniondenis@ya.ru>
2022-11-21 21:11:43 +0500

    HW6 is done

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,9 @@
+import scala.annotation.tailrec
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = if (seq.isEmpty) Nil else reverse(seq.tail) :+ seq.head
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +11,11 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = if (idx == 1 || idx == 2) 1 else fibonacci4Index(idx - 1) + fibonacci4Index(idx - 2)
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = (2 to idx).foldLeft(Seq(0, 1)){
+    (acc, i) => acc :+ acc(i - 1) + acc(i - 2)
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +23,27 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text
+    .toUpperCase()
+    .split("")
+    .map(ltr => MORSE.getOrElse(ltr, ltr))
+    .foldLeft(" ") {
+      (resStr, currLtr) => if (currLtr.last != '.' && currLtr.last != '-' && currLtr != " ")
+        resStr + currLtr
+      else
+        resStr + " " + currLtr
+    }
+    .trim
 
 
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = text
+    .split(" ")
+    .map(str => str.replaceAll("[а-яёА-ЯЁ]+", str.replaceAll("[^а-яёА-ЯЁ]+", "").reverse))
+    .map(str => {
+      if (str.replaceAll("[^а-яёА-ЯЁ]+", "").last.isUpper)
+        str.head.toUpper + str.tail.toLowerCase()
+      else
+        str
+    })
+    .mkString(" ")
 }
