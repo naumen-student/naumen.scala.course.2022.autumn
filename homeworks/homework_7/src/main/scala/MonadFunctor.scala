6e4b537f9facb2295b70d3708d7b09741ed361b9
Simon Z <saidelsam@ya.ru>
2022-12-10 21:39:51 +0500

    homework_7 Зайдельман Семён

--- homeworks/homework_7/src/main/scala/MonadFunctor.scala
+++ homeworks/homework_7/src/main/scala/MonadFunctor.scala
@@ -1,23 +1,44 @@
 
+private object Implicits {
+  implicit class MonadOps[A, F[_] : Monad](private val fa: F[A]) {
+    def map[B](f: A => B): F[B] = Monad[F].map(fa)(f)
+
+    def >>=[B](f: A => F[B]): F[B] = Monad[F].flatMap(fa)(f)
+  }
+}
+
 trait Monad[F[_]] {
+  self =>
 
-    def pure[A](a: A): F[A]
+  import Implicits._
 
-    def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
+  implicit val monad = self
 
-    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+  def pure[A](a: A): F[A]
 
-    def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+  def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
-    def compose[A, B, C](f: A => F[B])(g: B => F[C]): A => F[C] = ???
+  def map[A, B](fa: F[A])(f: A => B) = flatMap(fa)(f.andThen(pure))
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = fa >>= (a => fb.map(f(a, _)))
+
+  def sequence[A](fas: List[F[A]]): F[List[A]] =
+    fas.foldLeft(pure(List.empty[A]))((acc, fa) => fa >>= (a => acc.map(_.prepended(a)))).map(_.reverse)
+
+  def compose[A, B, C](f: A => F[B])(g: B => F[C]): A => F[C] = f.andThen(_ >>= g)
+
+}
+
+object Monad {
+  def apply[F[_] : Monad]: Monad[F] = implicitly[Monad[F]]
 }
 
 trait Functor[F[_]] {
-    def map[A, B](a: F[A])(f: A => B): F[B]
+  def map[A, B](a: F[A])(f: A => B): F[B]
 }
 
 object Functor {
-    def functorFromMonad[F[_]](M: Monad[F]): Functor[F] = new Functor[F] {
-        def map[A, B](a: F[A])(f: A => B): F[B] = ???
-    }
+  def functorFromMonad[F[_]](M: Monad[F]): Functor[F] = new Functor[F] {
+    def map[A, B](a: F[A])(f: A => B): F[B] = M.map(a)(f)
+  }
 }
