d6a5fdd68172ebc6c566d0f6d8afbed9ca7821fb
ninele7 <oleg.belohohlov01@gmail.com>
2022-12-11 14:34:58 +0500

    homework_6 - Белохохлов Олег

--- homeworks/homework_6/src/test/scala/Test.scala
+++ homeworks/homework_6/src/test/scala/Test.scala
@@ -1,30 +1,31 @@
 import utest._
 
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-  val tests = Tests{
-    'reverse - {
+  val tests = Tests {
+    test("reverse") {
       assert(Exercises.reverse(Seq(1, 2, 3)) == Seq(3, 2, 1))
       assert(Exercises.reverse(Seq(-1, -2, -3)) == Seq(-3, -2, -1))
     }
 
-    'fibonacci4Index - {
+    test("fibonacci4Index") {
       assert(Exercises.fibonacci4Index(2) == 1)
       assert(Exercises.fibonacci4Index(5) == 5)
     }
 
-    'fibonacci - {
+    test("fibonacci") {
       assert(Exercises.fibonacci(2) == Seq(0, 1, 1))
       assert(Exercises.fibonacci(5) == Seq(0, 1, 1, 2, 3, 5))
     }
 
-    'morse - {
+    test("morse") {
       assert(Exercises.morse("SOS") == "... --- ...")
-      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -..!")
+      assert(Exercises.morse("Hello world!") == ".... . .-.. .-.. ---   .-- --- .-. .-.. -.. !")
     }
 
-    'wordReverse - {
+    test("wordReverse") {
+      println(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя..."))
       assert(Exercises.wordReverse("Зима!.. Крестьянин, торжествуя...") == "Амиз!.. Ниняьтсерк, яувтсежрот...")
     }
 
