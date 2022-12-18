e294f290d9b04ba700376e57c0a80ac8e10439b2
NikiforovAlexandr04 <nikiforovaleksandr2001@mail.ru>
2022-12-18 16:33:52 +0500

    homework_7 Никифоров Александр

--- homeworks/homework_7/src/main/scala/MonadFunctor.scala
+++ homeworks/homework_7/src/main/scala/MonadFunctor.scala
@@ -5,11 +5,13 @@ trait Monad[F[_]] {
 
     def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B]
 
-    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+    private def map[A, B](fa: F[A])(f: A => B) = flatMap(fa)(f.andThen(pure))
 
-    def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+    def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = flatMap(fa)(a => map(fb)(f(a, _)))
 
-    def compose[A, B, C](f: A => F[B])(g: B => F[C]): A => F[C] = ???
+    def sequence[A](fas: List[F[A]]): F[List[A]] = fas.foldRight(pure(List.empty[A]))((fl, fr) => map2(fl, fr)(_ :: _))
+
+    def compose[A, B, C](f: A => F[B])(g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
 }
 
 trait Functor[F[_]] {
@@ -18,6 +20,6 @@ trait Functor[F[_]] {
 
 object Functor {
     def functorFromMonad[F[_]](M: Monad[F]): Functor[F] = new Functor[F] {
-        def map[A, B](a: F[A])(f: A => B): F[B] = ???
+        def map[A, B](a: F[A])(f: A => B): F[B] =  M.flatMap(a)(c => M.pure(f(c)))
     }
 }
