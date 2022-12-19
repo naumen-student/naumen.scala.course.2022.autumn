6f303e1a9d075460d757db189c5cdffe7a7b80dc
Grigorij_Kosov <kosov.gr@mail.ru>
2022-12-19 16:08:08 +0500

    done

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,35 @@
 import scala.util.Try
+import scala.util.matching.Regex
 object Exercises {
+  trait Read[T] {
+    def read(str: String): Either[String, T]
+  }
+
+  implicit class ReadOptions(private val value: String) {
+    def read[T](implicit reader: Read[T]): Either[String, T] = reader.read(value)
+  }
+
+  object Read {
+    def read[T](string: String)(implicit read: Read[T]): Either[String, T] = read.read(string)
+  }
+
+  implicit val strReader: Read[String] = (str: String) => Right(str)
+
+  implicit val intReader: Read[Int] = (str: String) => {
+    try {
+      Right(str.toInt)
+    } catch {
+      case e: NumberFormatException => Left(s"$str is not in correct format")
+    }
+  }
+  val someRegex: Regex = """Some\((\S+)\)""".r
+  implicit def optionReader[T: Read]: Read[Option[T]] = {
+    case "None" => Right(None)
+    case someRegex(value) => value.read[T] match {
+      case Right(x) => Right(Some(x))
+      case Left(x) => Left(s"$x can't be read")
+    }
+    case x => Left(s"$x can't be read")
+  }
 }
+
