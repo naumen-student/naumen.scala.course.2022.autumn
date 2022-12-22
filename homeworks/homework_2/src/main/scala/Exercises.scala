57cbe308b8b4a6295e7ac962e4808fe4484dde65
BozhkovKostya <bozhkovkost@gmail.com>
2022-12-22 21:31:04 +0500

    homework_2 - Божков Константин

--- homeworks/homework_2/src/main/scala/Exercises.scala
+++ homeworks/homework_2/src/main/scala/Exercises.scala
@@ -16,8 +16,9 @@ object Exercises {
     /*Реализовать функцию, которая возвращает сумму всех целых чисел в заданном диапазоне (от iForm до iTo), которые делятся
     на 3 или на 5.*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = ???
-
+    def sumOfDivBy3Or5(iFrom: Int, iTo: Int): Long = {
+        (iFrom to iTo).filter(i => i % 3 == 0 || i % 5 == 0).sum
+    }
 
 
     /*ЗАДАНИЕ II*/
@@ -25,7 +26,18 @@ object Exercises {
     Число 80 раскладывается на множители 1 * 2 * 2 * 2 * 2 * 5, результат выполнения функции => Seq(2, 5).
     Число 98 можно разложить на множители 1 * 2 * 7 * 7, результат выполнения функции => Seq(2, 7).*/
     /*Реализовать юнит-тесты в src/test/scala для данной функции.*/
-    def primeFactor(number: Int): Seq[Int] = ???
+    def primeFactor(number: Int): Seq[Int] = {
+        for (i <- 2 to number; if number % i == 0) {
+            return (Seq(i) ++ primeFactor(number / i)).distinct
+        }
+        Seq()
+    }
+
+    def isPrime(number: Int): Boolean = {
+        if (number == 1) return false
+        for (i <- 2 to number / 2 + 1) {if(number % i == 0) return false}
+        true
+    }
 
 
 
@@ -40,15 +52,13 @@ object Exercises {
     def abs(vec: Vector2D): Double = java.lang.Math.sqrt(vec.x * vec.x + vec.y * vec.y)
     def scalar(vec0: Vector2D, vec1: Vector2D): Double = vec0.x * vec1.x + vec0.y * vec1.y
     def cosBetween(vec0: Vector2D, vec1: Vector2D): Double = scalar(vec0, vec1) / abs(vec0) / abs(vec1)
-    //def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, ???, rightVec0: Vector2D, rightVec1: Vector2D) = ???
-    /*
+    def sumByFunc(leftVec0: Vector2D, leftVec1: Vector2D, func: (Vector2D, Vector2D) => Double, rightVec0: Vector2D, rightVec1: Vector2D) = {
+        func(leftVec0, leftVec1) + func(rightVec0, rightVec1)
+    }
     def sumScalars(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, scalar, rightVec0, rightVec1)
-    */
-    /*
     def sumCosines(leftVec0: Vector2D, leftVec1: Vector2D, rightVec0: Vector2D, rightVec1: Vector2D): Double =
         sumByFunc(leftVec0, leftVec1, cosBetween, rightVec0, rightVec1)
-    */
 
 
 
@@ -71,6 +81,12 @@ object Exercises {
             "Chrome" ->   (3,   7.18),   "Cesium" ->    (7,   1.873), "Zirconium" -> (3,   6.45)
         )
 
-    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = ???
+    def sortByHeavyweight(ballsArray: Map[String, (Int, Double)] = balls): Seq[String] = {
+        ballsArray.toSeq.sortBy((ball: (String, (Int, Double))) => {
+            val radius = ball._2._1
+            val density = ball._2._2
+            density * java.lang.Math.PI * java.lang.Math.pow(radius, 3) * 4 / 3
+        }).map(ball => ball._1)
+    }
 
 }
--- homeworks/homework_2/src/test/scala/Test.scala
+++ homeworks/homework_2/src/test/scala/Test.scala
@@ -1,12 +1,139 @@
+import Exercises.Vector2D
 import utest._
 
-object Test extends TestSuite{
+object Test extends TestSuite {
 
-    val tests = Tests{
+    val tests = Tests {
         'test_divBy3Or7 - {
             assert(Exercises.divBy3Or7(1, 3) == Seq(3))
             assert(Exercises.divBy3Or7(5, 9) == Seq(6, 7, 9))
-            assert(Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21, 24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60, 63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99))
+            assert(
+                Exercises.divBy3Or7(0, 100) == Seq(0, 3, 6, 7, 9, 12, 14, 15, 18, 21,
+                    24, 27, 28, 30, 33, 35, 36, 39, 42, 45, 48, 49, 51, 54, 56, 57, 60,
+                    63, 66, 69, 70, 72, 75, 77, 78, 81, 84, 87, 90, 91, 93, 96, 98, 99)
+            )
+            assert(Exercises.divBy3Or7(1, 2) == Seq())
+        }
+        'test_sumOfDivBy3Or5 - {
+            assert(Exercises.sumOfDivBy3Or5(0, 3) == 3L)
+            assert(Exercises.sumOfDivBy3Or5(1, 2) == 0L)
+            assert(Exercises.sumOfDivBy3Or5(5, 15) == 57L)
+            assert(Exercises.sumOfDivBy3Or5(2, 10) == 33L)
+        }
+        'test_primeFactor - {
+            assert(Exercises.primeFactor(80) == Seq(2, 5))
+            assert(Exercises.primeFactor(2) == Seq(2))
+            assert(Exercises.primeFactor(4) == Seq(2))
+            assert(Exercises.primeFactor(123) == Seq(3, 41))
+            assert(Exercises.primeFactor(78) == Seq(2, 3, 13))
+        }
+        'test_sumScalars - {
+            assert(
+                Exercises.sumScalars(
+                    Vector2D(0, 0),
+                    Vector2D(1, 0),
+                    Vector2D(0, 0),
+                    Vector2D(2, 2)
+                ) == 0.0
+            )
+            assert(
+                Exercises.sumScalars(
+                    Vector2D(0, 0),
+                    Vector2D(1, 1),
+                    Vector2D(2, 3),
+                    Vector2D(4, 5)
+                ) == 23.0
+            )
+            assert(
+                Exercises.sumScalars(
+                    Vector2D(1, 0),
+                    Vector2D(1, 0),
+                    Vector2D(2, 3),
+                    Vector2D(4, 5)
+                ) == 24.0
+            )
+        }
+        'test_sumCosines - {
+            assert(
+                Exercises
+                  .sumCosines(
+                      Vector2D(0, 0),
+                      Vector2D(0, 0),
+                      Vector2D(0, 0),
+                      Vector2D(0, 0)
+                  )
+                  .isNaN
+            )
+            assert(
+                Exercises.sumCosines(
+                    Vector2D(-4, -4),
+                    Vector2D(2, 2),
+                    Vector2D(3, 3),
+                    Vector2D(-1, -1)
+                ) == -2
+            )
+        }
+        'test_sortByHeavyweight - {
+            assert(
+                Exercises.sortByHeavyweight(
+                    Map(
+                        "Aluminum" -> (3, 2.6889),
+                        "Tungsten" -> (2, 19.35),
+                        "Graphite" -> (12, 2.1),
+                        "Iron" -> (3, 7.874),
+                        "Gold" -> (2, 19.32),
+                        "Potassium" -> (14, 0.862),
+                        "Calcium" -> (8, 1.55),
+                        "Cobalt" -> (4, 8.90),
+                        "Lithium" -> (12, 0.534),
+                        "Magnesium" -> (10, 1.738),
+                        "Copper" -> (3, 8.96),
+                        "Sodium" -> (5, 0.971),
+                        "Nickel" -> (2, 8.91),
+                        "Tin" -> (1, 7.29),
+                        "Platinum" -> (1, 21.45),
+                        "Plutonium" -> (3, 19.25),
+                        "Lead" -> (2, 11.336),
+                        "Titanium" -> (2, 10.50),
+                        "Silver" -> (4, 4.505),
+                        "Uranium" -> (2, 19.04),
+                        "Chrome" -> (3, 7.18),
+                        "Cesium" -> (7, 1.873),
+                        "Zirconium" -> (3, 6.45)
+                    )
+                ) ==
+                  Seq(
+                      "Tin",
+                      "Platinum",
+                      "Nickel",
+                      "Aluminum",
+                      "Titanium",
+                      "Lead",
+                      "Sodium",
+                      "Uranium",
+                      "Gold",
+                      "Tungsten",
+                      "Zirconium",
+                      "Chrome",
+                      "Iron",
+                      "Copper",
+                      "Silver",
+                      "Plutonium",
+                      "Cobalt",
+                      "Cesium",
+                      "Calcium",
+                      "Lithium",
+                      "Magnesium",
+                      "Potassium",
+                      "Graphite"
+                  )
+            )
+            assert(Exercises.sortByHeavyweight(Map()) == Seq())
+            assert(
+                Exercises.sortByHeavyweight(Map("Aluminum" -> (3, 2.6889))) == Seq(
+                    "Aluminum"
+                )
+            )
         }
     }
 }
