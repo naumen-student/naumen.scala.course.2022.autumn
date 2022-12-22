21a93d4ff3ac35dbb23c3d18d04e2136fd3be63f
Loraan <bfire2066@gmail.com>
2022-12-23 01:26:01 +0500

    homework_5 Головачёв Георгий

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,14 @@
+import scala.collection.mutable
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    var res: List[T] = List()
+    for (i <- seq.indices)
+      res = seq(i) :: res
+    res
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +16,30 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    var a: Int = 0
+    var b: Int = 1
+    for (i <- 0 until idx - 1) {
+      val temp = b
+      b = a + b
+      a = temp
+    }
+    b
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    if (idx == 0) return Seq(0)
+    var a: Int = 0
+    var b: Int = 1
+    var ans = Seq(fib1, fib2)
+    for (i <- 0 until idx - 1) {
+      val temp = b
+      b = a + b
+      ans = ans :+ b
+      a = temp
+    }
+    ans
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +47,32 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String = text.map(symb => if (MORSE contains symb.toUpper.toString) MORSE(symb.toUpper.toString) else symb).mkString(" ")
 
 
-  def wordReverse(text: String): String = ???
-
+  def wordReverse(text: String): String = {
+    val splitText = text.split(" ")
+    val ans: mutable.StringBuilder = new mutable.StringBuilder
+    for (line <- splitText) {
+      val toRev: mutable.StringBuilder = new mutable.StringBuilder
+      for (symb <- line) {
+        if (symb.isLetter) {
+          toRev += symb.toLower
+        }
+      }
+      val rev = toRev.reverse
+      if (line(0).isUpper) {
+        rev(0) = rev(0).toUpper
+      }
+      for (i <- line.indices) {
+        if (line(i).isLetter) {
+          ans += rev(i)
+        } else {
+          ans += line(i)
+        }
+      }
+      ans += ' '
+    }
+    ans.init.toString()
+  }
 }
