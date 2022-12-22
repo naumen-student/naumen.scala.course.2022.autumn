c483e634efad1d11e40858e021cf5e6a999eff64
arinabatyrbaeva <rina.batyrbaeva@mail.ru>
2022-12-22 19:09:09 +0500

    homework_8 - Батырбаева Арина

--- homeworks/homework_8/src/main/scala/Exercises.scala
+++ homeworks/homework_8/src/main/scala/Exercises.scala
@@ -1,3 +1,28 @@
 import scala.util.Try
+
 object Exercises {
+
+  trait Read[T] {
+    def read(string: String): Either[String, T]
+  }
+
+  object Read {
+    def read[T: Read](value: String)(implicit reader: Read[T]): Either[String, T] = reader.read(value)
+  }
+
+  implicit class OptionReader(value: String) {
+    def read[T](implicit reader: Read[T]): Either[String, T] = reader.read(value)
+  }
+
+  implicit val intReader: Read[Int] = (value: String) => Try(value.toInt).toOption.toRight(value)
+
+  implicit val stringReader: Read[String] = Right(_)
+
+  val rgx = """Some\((\S+)\)""".r
+
+  implicit def readOption[T: Read]: Read[Option[T]] = {
+    case "None" => Right(None)
+    case rgx(value) => value.read[T].map(Some(_))
+    case other => Left(other)
+  }
 }
