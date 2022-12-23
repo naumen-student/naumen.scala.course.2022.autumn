2d38c467b705ed3ba738bd70a2d872480249271f
mnoskov <mnoskov@naumen.ru>
2022-12-23 22:21:01 +0500

    HW6

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,14 @@
+
+
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    var result: List[T] = List()
+    for (i <- seq.indices)
+      result = seq(i) :: result
+    result
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +16,18 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    val fib = fibonacci(idx)
+    fib.last
+  }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+  def fibonacci(idx: Int): Seq[Int] = {
+    if (idx == 0) return Seq(0)
+    var fib = Seq(0, 1)
+    for (i <- 2 to idx)
+      fib = fib :+ fib.drop(fib.length - 2).sum
+    fib
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
@@ -19,9 +35,32 @@ object Exercises {
                        "S" -> "...", "T" -> "-", "U" -> "..-", "V" -> "...-", "W" -> ".--", "X" -> "-..-",
                        "Y" -> "-.--", "Z" -> "--..")
 
-  def morse(text: String): String = ???
+  def morse(text: String): String =
+    text.map(c => MORSE.getOrElse(c.toUpper.toString, c)).mkString(" ")
 
 
-  def wordReverse(text: String): String = ???
+  def wordReverse(text: String): String = {
+    val tokens = text.split(" ")
+    val res = new StringBuilder()
+    for (word <- tokens) {
+      val toReverse = new StringBuilder()
+      var ending = new StringBuilder()
+      for (symbol <- word)
+        if (symbol.isLetter)
+          toReverse.append(symbol.toLower)
+        else
+          ending.append(symbol)
+      var reversed = toReverse.reverse.toString
+
+      if (word(0).isUpper)
+        reversed = reversed(0).toUpper + reversed.substring(1)
+
+      for (i <- word.indices)
+        if (word(i).isLetter)
+          res.append(reversed(i))
+      res.append(ending).append(" ")
+    }
+    res.toString.trim
+  }
 
 }
